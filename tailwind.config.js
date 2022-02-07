b8ec6ec4c0845487d9f1f31564afd1f85b22fb80
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-07 21:20:48 +0700

    enabled dark mode on class

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // enabled: false,
     content: ["./*.html"]
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
